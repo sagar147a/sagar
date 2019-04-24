071693d0ab5b85792c7b295cd4ca6c27d06471c9
vrindasonal <vrindasonal@gmail.com>
2019-04-24 14:41:55 +0530

    ws1 feature1

--- src/Japp.java
+++ src/Japp.java
@@ -5,6 +5,7 @@ public class Japp {
 		System.out.println("Hello Git World");
 		System.out.println("line6 ws2");
 		System.out.println("line7 ws1");
+		System.out.println("line8 ws1 feature1");
 		
 	}
 
